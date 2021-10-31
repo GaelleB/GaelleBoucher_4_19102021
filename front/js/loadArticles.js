be3a9146b8de511910fbdbe583f2e90bf8f51f92
Gaelle Boucher <gaelleboucher23@gmail.com>
2021-10-31 18:40:29 +0100

    first commit

---
+++ front/js/loadArticles.js
@@ -0,0 +1,20 @@
+const allProductsURL = 'http://localhost:3000/api/products'
+
+fetch(allProductsURL)
+    .then(res => res.json())
+    .then(res => {
+
+        const itemsSection = document.querySelector('#items');
+        res.forEach((el) => {
+
+            const itemLink = document.createElement('a');
+            itemLink.href = `./front/html/product.html?id=${el.id}`
+            itemLink.innerHTML = `<article>
+                    <img src= ${el.imageUrl} alt =${el.altTxt}>
+                    <h3 class="productName"> ${el.name} </h3> 
+                    <p class="productDescription"> ${el.description} </p>
+                    </article >`
+            itemsSection.appendChild(itemLink)
+            return itemLink
+        })
+    })
