45ddd113375ae4fb6a8d524a7412ba681755731a
Gaelle Boucher <gaelleboucher23@gmail.com>
2021-11-17 10:54:49 +0100

    modif adresse url

--- front/js/getArticleId.js
+++ front/js/getArticleId.js
@@ -8,7 +8,7 @@ fetch(allProductsURL)
         const itemsSection = document.querySelector("#items");
         res.forEach((el) => {
             const itemLink = document.createElement("a");
-            itemLink.href = `./product.html?id = ${el._id}
+            itemLink.href = `./product.html?id=${el._id}
                         `;
             itemLink.innerHTML = ` <article>
                             <img src = ${el.imageUrl} alt = ${el.altTxt}>
