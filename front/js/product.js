0c44aec412f71916b57da53f3d23b091d72eed0a
Gaelle Boucher <gaelleboucher23@gmail.com>
2021-12-03 19:09:10 +0100

    affichage du nom, du prix et de la description

--- front/js/product.js
+++ front/js/product.js
@@ -12,7 +12,23 @@ const productUrl = `http://localhost:3000/api/products/${id}`;
 fetch(productUrl)
     .then((res) => res.json())
     .then((product) => {
+        // Affichage de l'image du produit
         let img = `<img src="${product.imageUrl}"/>`
         const image = document.getElementById("itemImg");
         image.innerHTML = img
+
+        // Affichage du nom du produit
+        let nom = `${product.name}`
+        const name = document.getElementById("title");
+        name.innerHTML = nom
+
+        // Affichage du prix du produit
+        let prix = `${product.price}`
+        const price = document.getElementById("price");
+        price.innerHTML = prix
+
+        // Affichage de la description du produit
+        let descrip = `${product.description}`
+        const description = document.getElementById("description");
+        description.innerHTML = descrip
     });
