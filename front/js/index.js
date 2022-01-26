f1c6ed49810996135a11325126b38f1adcbc551e
Gaëlle Boucher <gaelleboucher23@gmail.com>
2022-01-26 23:04:29 +0100

    maj index.js

--- front/js/index.js
+++ front/js/index.js
@@ -11,13 +11,17 @@ fetch(allProductsURL)
         const itemsSection = document.querySelector("#items");
         res.forEach((el) => {
             console.log(el._id)
+
             // Insersion de chaque produit dans la page d'accueil
+            
+            // Constantes pour créer des éléments HTML avec la méthode "document.createElement"
             const a = document.createElement("a");
             const section = document.createElement("article");
             const img = document.createElement("img");
             const titre = document.createElement("h3");
             const para = document.createElement("p");
 
+            // Ajout de leur class (avec classList.add), d'un texte (innerText) ou d'un attribut
             section.classList.add("items");
             img.classList.add("alt");
             titre.classList.add("productName")
@@ -29,6 +33,7 @@ fetch(allProductsURL)
             titre.innerText = el.name;
             para.innerText= el.description;
             
+            // Apparition dans le DOM et affichage des produits dans la page d'accueil
             itemsSection.appendChild(a)
             a.appendChild(section);
             section.append(img, titre, para);
